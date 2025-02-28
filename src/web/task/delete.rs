891af55a58c7840cbe52ca078fc081f6baa0e396
baguuc <janzukowski@proton.me>
2025-02-28 20:00:33 +0100

    update: added DELETE /tasks/{task_id} route

--- src/web/task/delete.rs
+++ src/web/task/delete.rs
@@ -1,31 +1,34 @@
-use actix_web::{http::StatusCode, post, web::{Data, Json}, Responder};
+use actix_web::{delete, http::StatusCode, web::{Data, Path}, Responder};
 use serde_json::json;
 use sqlx::PgPool;
 
-use crate::{models::task::insert::TaskInsertQuery, web::ServerResponse};
+use crate::{models::task::{delete::TaskDeleteQuery, list::TaskListQuery}, web::ServerResponse};
 
-#[post("/tasks")]
+#[delete("/tasks/{task_id}")]
 pub async fn controller(
-    task_query: Json<TaskInsertQuery>,
+    task_id: Path<i32>,
     db_conn: Data<PgPool>
 ) -> impl Responder {
     let mut db_conn = db_conn
         .acquire()
         .await
         .unwrap();
+    
+    let task_id = task_id.into_inner();
+    let task_query = TaskDeleteQuery::new(task_id);
 
     let task = task_query
         .execute(&mut db_conn)
         .await;
 
     match task {
-        Ok(task) => return ServerResponse::new(
+        Ok(tasks) => return ServerResponse::new(
             StatusCode::OK,
-            Some(json!(task))
+            None
         ),
         Err(err) => return ServerResponse::new(
             StatusCode::BAD_REQUEST,
             Some(json!(err.to_string()))
-        )
+        ) 
     }
 }
